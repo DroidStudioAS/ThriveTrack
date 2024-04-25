4f14e0295d8e1c322cd77085325953cff77f846a
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-25 11:27:57 +0200

    added url building logic to callGet function in NetworkHelper

--- app/src/main/java/com/aa/thrivetrack/fragments/auth/LoginFragment.java
+++ app/src/main/java/com/aa/thrivetrack/fragments/auth/LoginFragment.java
@@ -7,60 +7,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.aa.thrivetrack.R;
+import com.aa.thrivetrack.network.NetworkHelper;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button loginTrigger;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String[] PATH_TO_LOGIN = new String[]{"authentication","login"};
+    private static final String[] primer = new String[]{};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        /*****Start Of Ui Initializations****/
+        loginTrigger=view.findViewById(R.id.loginTrigger);
+        /*****End Of Ui Initializations****/
+
+        /*****Start Of OnClickListeners****/
+        loginTrigger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String ,String> params = new HashMap();
+
+                params.put("username",null);
+                params.put("password", null);
+
+
+
+                NetworkHelper.callGet(PATH_TO_LOGIN, params);
+            }
+        });
+        /*****End Of OnClickListeners****/
+
+        return view;
     }
 }
--- app/src/main/java/com/aa/thrivetrack/network/NetworkHelper.java
+++ app/src/main/java/com/aa/thrivetrack/network/NetworkHelper.java
@@ -1,20 +1,42 @@
 package com.aa.thrivetrack.network;
 
-public class NetworkHelper {
-    static final String BASE_URL = "http://339h123.f1eu.mars-hosting.com/";
+import android.util.Log;
 
-    public String buildUrl(String[] params){
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+
+public class NetworkHelper {
+    static final String BASE_URL = "http://339h123.f1eu.mars-hosting.com";
+
+    OkHttpClient client = new OkHttpClient();
+
+    public static String buildUrl(String[] routeParams){
         StringBuilder url = new StringBuilder();
         url.append(BASE_URL);
-        if(params.length>0) {
-            for (String path : params) {
+        if(routeParams.length>0) {
+            for (String path : routeParams) {
                 url.append("/" + path);
             }
         }
         return url.toString();
     }
 
-    public static void callGet(){
+    public static void callGet(String[] routeParams, Map<String,String> params){
+
+        String path = buildUrl(routeParams);
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(path).newBuilder();
+
+        for (Map.Entry<String,String> entry : params.entrySet()){
+            urlBuilder.addQueryParameter(entry.getKey(), entry.getValue());
+        }
+        String URL = urlBuilder.build().toString();
+
+        Log.i("url", URL);
+
+
 
     }
     public static void callPost(){
