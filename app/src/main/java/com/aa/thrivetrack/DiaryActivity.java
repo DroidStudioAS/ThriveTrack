bfde9e3c26cf7fb40cf15e76d0afdab289ea7d5a
DroidStudioAS <as.droiddevteam@gmail.com>
2024-05-03 11:41:01 +0200

    sending input data to server, and NOT rendering todays date in entry list

--- app/src/main/java/com/aa/thrivetrack/DiaryActivity.java
+++ app/src/main/java/com/aa/thrivetrack/DiaryActivity.java
@@ -7,16 +7,22 @@ import androidx.constraintlayout.widget.Guideline;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aa.thrivetrack.helpers.DateHelper;
 import com.aa.thrivetrack.models.Diary;
+import com.aa.thrivetrack.network.NetworkHelper;
 import com.aa.thrivetrack.network.SessionStorage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DiaryActivity extends AppCompatActivity {
 
@@ -28,6 +34,10 @@ public class DiaryActivity extends AppCompatActivity {
     Button saveTodaysInput;
 
     private SharedPreferences sharedPreferences;
+    boolean todayEntered;
+
+    private static final String[] PATH_TO_INSERT_ENTRY = new String[]{"write","diary"};
+    private static final String[] PATH_TO_UPDATE_ENTRY = new String[]{"edit","patch","diary"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +53,46 @@ public class DiaryActivity extends AppCompatActivity {
 
         sharedPreferences=getSharedPreferences(SessionStorage.getUsername(), MODE_PRIVATE);
         String todaysInput = sharedPreferences.getString("todays_diary", "");
+        todayEntered=sharedPreferences.getBoolean("diary_entered", false);
         diaryInput.setText(todaysInput);
 
         saveTodaysInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String entryText = diaryInput.getText().toString().trim();
+                if(entryText.equals("") || entryText.equals(todaysInput)){
+                    return;
+                }
+                Map<String,String> params = new HashMap<>();
+                params.put("date", DateHelper.buildTodaysDate());
+                params.put("user-id", String.valueOf(SessionStorage.getUserData().getUser().getUser_id()));
+                params.put("entry-text", entryText);
+                Log.i("date", DateHelper.buildTodaysDate());
+                Log.i("user-id", String.valueOf(SessionStorage.getUserData().getUser().getUser_id()));
+                Log.i("entry-text", entryText);
+
+                String date =sharedPreferences.getString("date","");
+                String[] routePath = !todayEntered ? PATH_TO_INSERT_ENTRY : PATH_TO_UPDATE_ENTRY;
+                if(todayEntered){
+                    NetworkHelper.callPatch(routePath, params, 0);
+                }else{
+                    NetworkHelper.callPost(routePath, params, 0);
+                }
+                NetworkHelper.waitForReply();
+
+                Log.e("response", SessionStorage.getServerResponse());
+
                 SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                if(SessionStorage.getServerResponse().equals("true")){
+                    Toast.makeText(getApplicationContext(), "Diary Entry Added", Toast.LENGTH_SHORT).show();
+                    editor.putBoolean("diary_entered", true);
+                }
+
                 editor.putString("todays_diary", diaryInput.getText().toString());
                 editor.apply();
+                SessionStorage.resetServerResponse();
+
             }
         });
         populateUi();
@@ -64,6 +106,9 @@ public class DiaryActivity extends AppCompatActivity {
         dateTv.setText(DateHelper.buildTodaysDate());
 
         for(Diary entry : SessionStorage.getUserData().getDiary()){
+            if(entry.getEntry_date().equals(DateHelper.buildTodaysDate())){
+                continue;
+            }
             TextView dateView = new TextView(this);
             TextView diaryText = new TextView(this);
             //generate ids
