08e88fbe7f7dcfc48fa904ba38626d6500024e79
DroidStudioAS <as.droiddevteam@gmail.com>
2024-05-06 11:52:05 +0200

    added calendar view and special decoration for days with a streak acomplished

--- app/src/main/java/com/aa/thrivetrack/ProfileActivity.java
+++ app/src/main/java/com/aa/thrivetrack/ProfileActivity.java
@@ -1,7 +1,11 @@
 package com.aa.thrivetrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,9 +18,15 @@ import com.aa.thrivetrack.helpers.DialogHelper;
 import com.aa.thrivetrack.models.Data;
 import com.aa.thrivetrack.models.User;
 import com.aa.thrivetrack.network.SessionStorage;
+import com.applandeo.materialcalendarview.CalendarDay;
 import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 
+import java.time.Year;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity implements PatchCallback {
 
@@ -40,7 +50,7 @@ public class ProfileActivity extends AppCompatActivity implements PatchCallback 
         changePasswordTrigger=(TextView)findViewById(R.id.changePasswordTrigger);
         deleteTrigger=(TextView)findViewById(R.id.deleteAcountTrigger);
         streakTv = (TextView) findViewById(R.id.userStreakTv);
-        calendarView=(CalendarView)findViewById(R.id.calendarView);
+        calendarView=(CalendarView) findViewById(R.id.calendarView);
         /******End Of Ui Initializations******/
         String streakString = "You Have A " + String.valueOf(SessionStorage.getUserData().getUser().getUser_streak()) + " Day Streak";
         streakTv.setText(streakString);
@@ -59,7 +69,28 @@ public class ProfileActivity extends AppCompatActivity implements PatchCallback 
 
         ArrayList<String> datesInRange = DateHelper.datesInRange(SessionStorage.getUserData().getStreaks().get(0).getStreak_start(),
                 SessionStorage.getUserData().getStreaks().get(0).getStreak_end());
+        List<CalendarDay> cd = new ArrayList<>();
+        for(String x : datesInRange){
+            String[] parsed = x.split("-");
+            int year = Integer.parseInt(parsed[0]);
+            int month = Integer.parseInt(parsed[1])-1;
+            int day = Integer.parseInt(parsed[2]);
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year,month,day);
 
+            CalendarDay calendarDay = new CalendarDay(calendar);
+
+            @SuppressLint("UseCompatLoadingForDrawables")
+            Drawable drawable = getDrawable(R.drawable.ic_streak);
+            //calendarDay.setBackgroundDrawable(drawable);
+            calendarDay.setLabelColor(R.color.accentgreen);
+            cd.add(calendarDay);
+
+
+
+        }
+
+        calendarView.setCalendarDays(cd);
 
         /******End Of OnClickListeners******/
         setProfilePageContent();
