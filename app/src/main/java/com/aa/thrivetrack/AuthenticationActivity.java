868963e8a4ec15b0bb1636343a1ed0781c9cd62c
DroidStudioAS <as.droiddevteam@gmail.com>
2024-05-07 15:01:04 +0200

    added transitions between launch screen and login register fragment

--- app/src/main/java/com/aa/thrivetrack/AuthenticationActivity.java
+++ app/src/main/java/com/aa/thrivetrack/AuthenticationActivity.java
@@ -1,18 +1,26 @@
 package com.aa.thrivetrack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.aa.thrivetrack.adapters.AuthenticationAdapter;
+import com.aa.thrivetrack.fragments.auth.LoginFragment;
+import com.aa.thrivetrack.fragments.auth.RegisterFragment;
 import com.google.android.material.tabs.TabLayout;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
-    private ViewPager viewPager;
-    private AuthenticationAdapter authAdapter;
-    private TabLayout tabLayout;
+    TextView loginButton;
+    TextView signUpButton;
+
+    FragmentContainerView fragmentContainerView;
+
 
 
 
@@ -21,8 +29,42 @@ public class AuthenticationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+        /**Start Of Ui Initialization**/
+        loginButton=(TextView) findViewById(R.id.loginButton);
+        signUpButton=(TextView) findViewById(R.id.signUpButton);
+        fragmentContainerView=(FragmentContainerView) findViewById(R.id.authFragmentContainer);
+        /**End Of Ui Initialization**/
+        loginButton.setOnClickListener(displayFragment(new LoginFragment()));
+        signUpButton.setOnClickListener(displayFragment(new RegisterFragment()));
 
+    }
 
+    public View.OnClickListener displayFragment(Fragment fragment){
+       return new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               fragmentContainerView.setVisibility(View.VISIBLE);
+               getSupportFragmentManager().
+                       beginTransaction()
+                       .addToBackStack(null)
+                       .replace(R.id.authFragmentContainer, fragment)
+                       .commit();
+               fragmentContainerView.bringToFront();
+
+               loginButton.setOnClickListener(null);
+               signUpButton.setOnClickListener(null);
+           }
+       };
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if(fragmentContainerView.getVisibility()==View.VISIBLE){
+            fragmentContainerView.setVisibility(View.GONE);
+            loginButton.setOnClickListener(displayFragment(new LoginFragment()));
+            signUpButton.setOnClickListener(displayFragment(new RegisterFragment()));
+        }
 
     }
 }
